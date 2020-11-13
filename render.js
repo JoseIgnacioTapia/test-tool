f756a64830438cada08472298dd6be4c82eb2ed4
José Ignacio <josignaciot@gmail.com>
2020-11-13 17:51:05 -0300

    An Incorrectly Passing Test

--- render.js
+++ render.js
@@ -2,7 +2,7 @@ const path = require('path');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-const render = (filename) => {
+const render = async (filename) => {
   const filePath = path.join(process.cwd(), filename);
 
   const dom = await JSDOM.fromFile(filePath, {
